d3eb1fd2d07dc67c857a383c0c8d0f760a8b861f
DESKTOP-L3T2LBI\Анастасия <anastacia2014fanipol@gmail.com>
2023-12-08 16:16:02 +0300

    Добавлены методы addObject, removeObject, findDuplicates

--- src/StringOperations.java
+++ src/StringOperations.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class StringOperations {
+import java.io.*;
+import java.util.*;
+
+public class StringOperations {
+    private ArrayList<String> stringList;
+
+    public StringOperations() {
+        this.stringList = new ArrayList<>();
+    }
+
+    // Добавление и удаление объектов
+    public void addObject(String str) {
+        stringList.add(str);
+    }
+
+    public void removeObject(String str) {
+        stringList.remove(str);
+    }
+
+    // Поиск одинаковых элементов с подсчетом совпадений
+    public Map<String, Integer> findDuplicates() { //возвращает отображение (Map), где ключи - это строки, а значения - целые числа (число повторений).
+        Map<String, Integer> frequencyMap = new HashMap<>();
+        for (String str : stringList) {
+            frequencyMap.put(str, frequencyMap.getOrDefault(str, 0) + 1);
+        }
+        return frequencyMap;
+    }
+
 }
